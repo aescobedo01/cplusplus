d59763a34e9e8d2074ed57e9d41e75d38848d52d
anaescobedo <ana.escbedo@gmail.com>
2025-01-16 16:50:07 -0600

    adding stuff to main

--- class_roster/main.cpp
+++ class_roster/main.cpp
@@ -23,8 +23,42 @@ int main()
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
         "A5,Ana,Escobedo,myemailaddress@gmail.com,23,20,21,22,SOFTWARE"
     };
- //hello world
-//helllo
 
-//hellooo
+// classroster is an instance of roster class
+//using pointer to access members of a class
+Roster* classRoster = new Roster();
+
+//for loop to parse through studentData
+for (int i = 0; i < 5; i++) {
+    classRoster->parse(studentData[i]);
+}
+
+//print all
+classRoster->printAll();
+
+//print all invalid emails
+classRoster->printInvalidEmails();
+
+//loop through classRosterArray to find avg num of days in each course
+for (int i = 0; i < 5; i++) {
+    classRoster->printAverageDaysInCourse(classRoster->GetId(i));
+}
+
+//separate each student by degree type
+classRoster->printByDegreeProgram(SECURITY);
+classRoster->printByDegreeProgram(NETWORK);
+classRoster->printByDegreeProgram(SOFTWARE);
+
+//REMOVE STUDENT A3, then print everyone except a3
+classRoster->remove("A3");
+classRoster->printAll();
+
+//parse again and it will say student a3 not found
+classRoster->remove("A3");
+
+delete classRoster;
+
+system("pause>0");
+
+return 0;
 }
